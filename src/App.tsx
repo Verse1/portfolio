7b5c17f1c1396ff55a6f2dc70e4a0e91071819d7
Verse <verseow@gmail.com>
2022-04-03 11:43:03 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+
+function SCE() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <OrbitControls />
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+    </Canvas>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <SCE />
+    </div>
+  );
+}
+
+export default App;
