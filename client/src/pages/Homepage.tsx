c7b62d2c20cd9a4c7d2ae483ab740b01fcc742cc
Verse <verseow@gmail.com>
2022-04-19 06:11:15 -0400

    added text

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,9 +1,10 @@
-import { Canvas } from '@react-three/fiber';
+import { Canvas, extend } from '@react-three/fiber';
 import {
   MeshDistortMaterial,
   OrbitControls,
   Sphere,
   Stars,
+  Text,
 } from '@react-three/drei';
 import { Suspense } from 'react';
 
@@ -24,6 +25,16 @@ function SCE() {
               speed={2}
             />
           </Sphere>
+
+          <Text scale={[10, 10, 10]} position={[3, 3, 0]} color="red">
+            Skills
+            <MeshDistortMaterial
+              color={'#6c2f94'}
+              roughness={0}
+              distort={0.75}
+              speed={1.4}
+            />
+          </Text>
         </Suspense>
       </Canvas>
     </div>
