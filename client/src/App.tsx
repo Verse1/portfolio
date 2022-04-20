59739f6ce37dd64e7c997630c478274692f7b5fb
Verse <verseow@gmail.com>
2022-04-20 03:09:36 -0400

    created basic admin dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import Homepage from './pages/Homepage';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Navbar from './components/Navbar';
+import Admin from './pages/Admin';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Homepage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   );
