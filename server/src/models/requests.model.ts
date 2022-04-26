0172e17ac9f3a5588abce369a73b6d4da9aaf775
Verse <verseow@gmail.com>
2022-04-26 07:42:35 -0400

    made request model

---
+++ server/src/models/requests.model.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const requestSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model('Request', requestSchema);
